334cdf721efd723593b6f00147dc26d5364e60c2
Derek Marion <derek.j.marion@gmail.com>
2023-10-12 09:11:29 -0700

    Step 3: load data into Student class and define all_students method and class variable to return an array of each student

--- classes/student.py
+++ classes/student.py
@@ -1,5 +1,19 @@
-from Person import Person
+from classes.person import Person #notice you have to specify the top level package/folder here, it's not implicit that the modules are in the same package when import to a module in another folder
+import csv
+
 class Student(Person):
     def __init__(self, name, age, role, password, school_id) -> None:
         super().__init__(name, age, role, password)
         self.school_id = school_id
+
+    @classmethod
+    def load_students(cls):
+        cls.students = [] #Notice this is a class variable, not an instance variable, so it will be available to all student instances, not declared above init because we don't necessarily want to initalize it when we creat a new student, only when loading all students
+        with open("data/students.csv", mode="r") as csv_file:  #filepath is relative to where the method is called
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                new_student = Student(name=row['name'], age=row['age'], role=row['role'], password=row['password'], school_id=row['school_id'])
+                cls.students.append(new_student)
+                
+    def all_students(cls):
+        return cls.students
--- runner.py
+++ runner.py
@@ -1,5 +1,11 @@
-from school import School 
+from classes.school import School 
+from classes.student import Student
+from classes.person import Person
 
-school = School('Ridgemont High') 
+# school = School('Ridgemont High') 
 
-print(school.name)
+# print(school.name)
+
+student = Student("name", "age", "role", "password", "school_id")
+student.load_students() #need parenthesis to actually call the method
+print(student.all_students()) #same here, notice how if you leave out parenthesis you get the method itself, not the method call
